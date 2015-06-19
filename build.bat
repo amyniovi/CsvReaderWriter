5cbb5e8b7de2cad1d07e39a5e19ea4634bb8cdb7
KateM <kate.macdonald@thetrainline.com>
2015-06-19 14:00:54 +0100

    Updating build.bat to do a package restore

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-msbuild .\src\AddressProcessor\AddressProcessing.csproj
-msbuild .\src\AddressProcessor.Tests\AddressProcessing.Tests.csproj
+nuget restore src\Exercise.sln
+msbuild src\AddressProcessor\AddressProcessing.csproj
+msbuild src\AddressProcessor.Tests\AddressProcessing.Tests.csproj
